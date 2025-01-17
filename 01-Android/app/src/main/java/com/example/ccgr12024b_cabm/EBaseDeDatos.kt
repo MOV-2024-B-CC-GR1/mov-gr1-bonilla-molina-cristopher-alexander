0992f517477d015f5cb135f4263d4d5bd19e9af6
CristopherBonilla <cristopher.bonilla@epn.edu.ec>
2025-01-17 08:53:08 -0500

    Actualizaciones

---
+++ 01-Android/app/src/main/java/com/example/ccgr12024b_cabm/EBaseDeDatos.kt
@@ -0,0 +1,7 @@
+package com.example.ccgr12024b_cabm;
+
+public class EBaseDeDatos {
+    companion object {
+        val tablaEntrenador: ESqliteHelperEntrenador? = null
+    }
+}
---
+++ 01-Android/app/src/main/java/com/example/ccgr12024b_cabm/FRecyclerViewAdaptadorNombreDescripcion.kt
@@ -0,0 +1,29 @@
+package com.example.ccgr12024b_cabm
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+class FRecyclerViewAdaptadorNombreDescripcion(
+    private val contexto: FRecyclerView,
+    private val lista: ArrayList<BEntrenador>,
+    private val recyclerView: RecyclerView
+): RecyclerView.Adapter<
+        FRecyclerViewAdaptadorNombreDescripcion.MyViewHolder
+        >() {
+            inner class MyViewHolder(
+                view: View): RecyclerView.ViewHolder(view){}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
